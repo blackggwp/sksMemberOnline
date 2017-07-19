757606ec2b4a259d87b92bdd12dad18c7c0cd413
black <ironhighh@gmail.com>
2017-07-19 09:33:46 +0700

    fix footer

--- footer.php
+++ footer.php
@@ -1,21 +1,28 @@
-<div class="main_footer" height:200px;">
-<h1>Footer</h1>
-<div >
+<div class="main_footer" style="display: -webkit-inline-box;width: 100%;">
+<div>
 		<a href="index.php">Home</a>
 		<a href="register.php">register</a>
 		<a href="coupon.php">coupon</a>
   </div>
-  </div>
-<p><span class="main_footer" style="text-align: center;height:200px;"><a href="index.php"><img src="img/skslogo.png" alt="logo" width="134" height="83" style="float:left;" /></a></span>
-  
-<span>Follow Us</span><br>
-  
-  <span><img src="img/fb.png" width="30" height="30" /><img src="img/gg.png" width="29" height="30" /><img src="img/tw.png" width="30" height="30" /></span><img src="img/ig.png" width="29" height="30" /><img src="img/yt.png" width="30" height="30" /><img src="img/line-icon-1.png" width="32" height="32" /></p>
-  
-<p>Contact Us</p>
-<span style="float:right;">
-<p>Contact Us</p>
-<p>����ѷ �١Ԫ� �Թ������� �ӡѴ</p>
-<p> �� : 02-785-2899</p>
 
-</span>
+<div class="" style="width:30%;">
+	<p><span class="main_footer" style="height:200px;"><a href="index.php"><img src="img/skslogo.png" alt="logo" width="134" height="83" style="float:left;" /></a></span>
+</div>
+
+<div class="" style="width:30%;">
+	<span>Follow Us</span><br>
+  
+  	<span><img src="img/fb.png" width="30" height="30" /><img src="img/gg.png" width="29" height="30" /><img src="img/tw.png" width="30" height="30" /></span><img src="img/ig.png" width="29" height="30" /><img src="img/yt.png" width="30" height="30" /><img src="img/line-icon-1.png" width="32" height="32" /></p>
+  	<p>Contact Us</p>
+</div>
+
+
+<div class="" style="width:30%;">
+	<span>
+		<p>Contact Us</p>
+		<p>บริษัท ซูกิชิ อินเตอร์กรุ๊ป จำกัด</p>
+		<p>โทร : 02-785-2899</p>
+	</span>
+</div>
+
+</div>
